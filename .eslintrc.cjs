90d57c66ce3d914fd408036d50396a0299b23f65
MauroChapero <chaperomauro1803@gmail.com>
2023-05-02 18:02:38 -0300

    Initicializacion del proyecto

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+  },
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Cinefilo Movie Database
